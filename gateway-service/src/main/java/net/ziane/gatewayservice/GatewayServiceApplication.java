d3affa7bd2e7137a3a22dec89390978437bad7d1
unknown <ziane.aymn@gmail.com>
2025-11-20 04:06:13 +0100

    init

--- gateway-service/src/main/java/net/ziane/gatewayservice/GatewayServiceApplication.java
+++ gateway-service/src/main/java/net/ziane/gatewayservice/GatewayServiceApplication.java
@@ -8,10 +8,10 @@ import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class GateweyServiceApplication {
+public class GatewayServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(GateweyServiceApplication.class, args);
+        SpringApplication.run(GatewayServiceApplication.class, args);
     }
 
     @Bean
